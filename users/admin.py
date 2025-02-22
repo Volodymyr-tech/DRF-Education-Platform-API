eb3f3c4bc53c6dda6650dd59880dc42faba6d699
Valdemar <vovateslenko.1996.28@gmail.com>
2025-02-22 20:40:48 +0100

    Admin.register

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from users.models import CustomUser
+
+
 # Register your models here.
+@admin.register(CustomUser)
+class YourModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'is_staff', 'is_superuser')
