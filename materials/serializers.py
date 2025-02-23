9cc181d3e2eb56294a812d63639ac9a82d933cd1
Valdemar <vovateslenko.1996.28@gmail.com>
2025-02-24 00:46:47 +0100

    Added SerializerMethodField() to CourseSerializer

--- materials/serializers.py
+++ materials/serializers.py
@@ -2,9 +2,14 @@ from rest_framework import serializers
 from .models import Course, Lesson
 
 class CourseSerializer(serializers.ModelSerializer):
+    quantity_lessons = serializers.SerializerMethodField()
+
     class Meta:
         model = Course
         fields = '__all__'
+
+    def get_quantity_lessons(self, instance):
+        return instance.lessons.count() #instatse is the course object and 'lessons' is the related name in the Course model
 
 
 class LessonSerializer(serializers.ModelSerializer):
--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from .models import CustomUser
 from .serializers import UserProfileSerializer
 
+
 class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = UserProfileSerializer
